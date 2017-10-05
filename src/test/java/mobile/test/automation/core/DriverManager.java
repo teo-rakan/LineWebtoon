0e9114b79475724624fa8428c41fae4a7a99aa26
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-10-05 14:13:32 +0300

    drop unused imports

--- src/test/java/mobile/test/automation/core/DriverManager.java
+++ src/test/java/mobile/test/automation/core/DriverManager.java
@@ -1,10 +1,7 @@
 package mobile.test.automation.core;
 
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
